fdf2b27ab910b91c895d9845896244438ff139d0
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-05-17 13:11:26 +0200

    destinacije, paketi, headerPonuda

--- index.php
+++ index.php
@@ -9,4 +9,66 @@
     </article>
 </header>
 
+<!-- destinacije -->
+<section class="destinacije container">
+    <h2>Najpopularnije destinacije</h2>
+    <p>Rezervišite do kraja meseca za 30% popusta</p>
+    <article>
+        <div>
+            <div class="img">
+                <img src="img/grcka.jpg" alt="">
+            </div>
+            <div class="text">
+                <i class="fas fa-map-marker-alt"></i>
+                <span>Grčka</span>
+            </div>
+        </div>
+        <div>
+            <div class="img">
+                <img src="img/italija.jpg" alt="">
+            </div>
+            <div class="text">
+                <i class="fas fa-map-marker-alt"></i>
+                <span>Italija</span>
+            </div>
+        </div>
+        <div>
+            <div class="img">
+                <img src="img/kanada.jpg" alt="">
+            </div>
+            <div class="text">
+                <i class="fas fa-map-marker-alt"></i>
+                <span>Kanada</span>
+            </div>
+        </div>
+        <div>
+            <div class="img">
+                <img src="img/norveska.jpg" alt="">
+            </div>
+            <div class="text">
+                <i class="fas fa-map-marker-alt"></i>
+                <span>Norveška</span>
+            </div>
+        </div>
+        <div>
+            <div class="img">
+                <img src="img/rusija.jpg" alt="">
+            </div>
+            <div class="text">
+                <i class="fas fa-map-marker-alt"></i>
+                <span>Rusija</span>
+            </div>
+        </div>
+        <div>
+            <div class="img">
+                <img src="img/spanija.jpg" alt="">
+            </div>
+            <div class="text">
+                <i class="fas fa-map-marker-alt"></i>
+                <span>Španija</span>
+            </div>
+        </div>
+    </article>
+</section>
+
 <?php include 'footer.php'; ?>
--- ponuda.php
+++ ponuda.php
@@ -1,5 +1,43 @@
 <?php include 'header.php'; ?>
 
-<h1 style="font-size: 10rem; text-align: center;">Ponuda</h1>
+<!-- headerPonuda -->
+<header class="headerPonuda">
+    <article class="container">
+        <h1>Popust 10-30%</h1>
+        <p>Uplata do januara 2021.</p>
+        <button>Pretražite</button>
+    </article>
+</header>
+
+<!-- paketi -->
+<section class="paketi container">
+    <h2>Iskoristite promotivne pakete</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus enim ducimus a fugit repudiandae voluptates, necessitatibus cupiditate praesentium deleniti libero!</p>
+    <article>
+        <div>
+            <img src="img/ponuda1.jpg" alt="">
+        </div>
+        <div>
+            <div class="row">
+                <div class="img">
+                    <img src="img/ponuda2.jpg" alt="">
+                </div>
+                <div class="text">
+                    <h4>Najbolje cene</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, tempora rem! Mollitia aliquam officiis nisi cupiditate adipisci error unde fuga.</p>
+                </div>
+            </div>
+            <div class="row"> 
+                <div class="text">
+                    <h4>Najbolje lokacije</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, tempora rem! Mollitia aliquam officiis nisi cupiditate adipisci error unde fuga.</p>
+                </div>
+                <div class="img">
+                    <img src="img/ponuda3.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </article>
+</section>
 
 <?php include 'footer.php'; ?>
